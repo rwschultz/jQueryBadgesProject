a049c42d07d74d935c0231131ef4863ae99305e0
Ryan Schultz <rschultz@fandango.com>
2016-12-08 16:54:20 -0800

    Everything works

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
-
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Schultzinator.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach( course => {
+        $( "#badges" ).append( `<div class="course"><h3>${course.title}</h3><a class="btn btn-primary" href="${course.url}" target="_blank"><img src="${course.badge}" /></a></div>`);
+      })
+    }
+  });
 
 });
